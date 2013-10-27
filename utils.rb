eb54f82511e1596b58086633a562016c1d0742d2
James McKinney <james@slashpoundbang.com>
2013-10-27 09:47:42 -0400

    add unzip method

--- utils.rb
+++ utils.rb
@@ -2,7 +2,25 @@ require 'bundler/setup'
 
 require 'csv'
 
+require 'htmlentities'
 require 'pupa'
+require 'zip/zip'
 
 class GovernmentProcessor < Pupa::Processor
+  def unzip(url)
+    Tempfile.open('pupa') do |f|
+      f.binmode
+      f.write(get(url))
+      f.rewind
+
+      Zip::ZipFile.open(f) do |zipfile|
+        yield zipfile
+      end
+    end
+  end
+end
+
+class Pupa::Post
+  attr_accessor :area
+  dump :area
 end
