ab0c41db8d3622fb863597ef523e38dd827e55f1
James McKinney <james@slashpoundbang.com>
2013-12-28 19:55:08 -0500

    add Document and DownloadStore classes

--- utils.rb
+++ utils.rb
@@ -3,6 +3,7 @@ require 'bundler/setup'
 require 'csv'
 
 require 'active_model'
+require 'nokogiri'
 require 'pupa'
 require 'zip/zip'
 
@@ -23,10 +24,12 @@ end
 class Pupa::Person
   include ActiveModel::Validations
 
+  validates_inclusion_of :gender, in: %w(male female)
+
+  # @note Montreal-specific.
   validates_inclusion_of :honorific_prefix, in: %w(Monsieur Madame)
   validates_format_of :email, with: /\A[a-z.-]+@ville.montreal.qc.ca\z/, allow_blank: true
   validates_format_of :image, with: %r{\Ahttp://ville.montreal.qc.ca/pls/portal/docs/PAGE/COLLECTIONS_GENERALES/MEDIA/Images/Public/[\w-]+\.(?:JPG|jpg)\z}
-  validates_inclusion_of :gender, in: %w(male female)
   validate :validate_contact_details
 
   def validate_contact_details
@@ -96,6 +99,143 @@ class Pupa::Post
   dump :area, :position
 
   def fingerprint
-    super.slice(:label, :organization_id, :end_date, :position) # add position
+    super.slice(:label, :organization_id, :end_date, :position) # adds position
+  end
+end
+
+class Document
+  include Pupa::Model
+  include Pupa::Concerns::Timestamps
+  include Pupa::Concerns::Sourceable
+  include ActiveModel::Validations
+
+  attr_accessor :date, :description, :title, :organization_id
+  dump :date, :description, :title, :organization_id
+
+  # @note Montreal-specific.
+  validates_inclusion_of :description, in: [
+    'Assemblée extraordinaire',
+    'Assemblée ordinaire',
+    'Assemblée spéciale',
+    'Séance extraordinaire',
+    'Séance ordinaire',
+    'Séance spéciale',
+  ]
+
+  def fingerprint
+    {date: date, 'sources.url' => source_url}
+  end
+
+  def source_url
+    sources[0][:url]
+  end
+
+  def to_s
+    source_url
+  end
+end
+
+# Stores data downloads on disk.
+#
+# @see ActiveSupport::Cache::FileStore
+class DownloadStore
+  # @param [String] output_dir the directory in which to download data
+  def initialize(output_dir)
+    @output_dir = output_dir
+    FileUtils.mkdir_p(@output_dir)
+  end
+
+  # Returns whether a file with the given name exists.
+  #
+  # @param [String] name a key
+  # @return [Boolean] whether the store contains an entry for the given key
+  def exist?(name)
+    File.exist?(path(name))
+  end
+
+  # Returns all file names in the storage directory.
+  #
+  # @return [Array<String>] all keys in the store
+  def entries
+    Dir.chdir(@output_dir) do
+      Dir['*']
+    end
+  end
+
+  # Returns the contents of the file with the given name.
+  #
+  # @param [String] name a key
+  # @return [Hash] the value of the given key
+  def read(name)
+    File.open(path(name)) do |f|
+      f.read
+    end
+  end
+
+  # Returns the contents of the files with the given names.
+  #
+  # @param [String] names keys
+  # @return [Array<Hash>] the values of the given keys
+  def read_multi(names)
+    names.map do |name|
+      read(name)
+    end
+  end
+
+  # Writes the value to a file with the given name.
+  #
+  # @param [String] name a key
+  # @param [Hash,String] value a value
+  def write(name, value)
+    File.open(path(name), 'w') do |f|
+      f.write(value)
+    end
+  end
+
+  # Writes the value to a file with the given name, unless such a file exists.
+  #
+  # @param [String] name a key
+  # @param [Hash] value a value
+  # @return [Boolean] whether the key was set
+  def write_unless_exists(name, value)
+    !exist?(name).tap do |exists|
+      write(name, value) unless exists
+    end
+  end
+
+  # Writes the values to files with the given names.
+  #
+  # @param [Hash] pairs key-value pairs
+  def write_multi(pairs)
+    pairs.each do |name,value|
+      write(name, value)
+    end
+  end
+
+  # Delete a file with the given name.
+  #
+  # @param [String] name a key
+  def delete(name)
+    File.delete(path(name))
+  end
+
+  # Deletes all files in the storage directory.
+  def clear
+    Dir[File.join(@output_dir, '*')].each do |path|
+      File.delete(path)
+    end
+  end
+
+  # Collects commands to run all at once.
+  def pipelined
+    yield
+  end
+
+  # Returns the path to the file with the given name.
+  #
+  # @param [String] name a key
+  # @param [String] a path
+  def path(name)
+    File.join(@output_dir, name)
   end
 end
