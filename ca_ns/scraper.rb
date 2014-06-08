01f96996ae6322ae5735fc189052fdd3587567e6
James McKinney <james@slashpoundbang.com>
2014-06-08 17:59:32 -0400

    start state machine

--- ca_ns/scraper.rb
+++ ca_ns/scraper.rb
@@ -4,12 +4,97 @@ class NovaScotia < GovernmentProcessor
   def initialize(*args)
     super
 
+    @state = @initial_state
+
+    # If the machine is in a state ending in "_begin", the next state is
+    # expected to be that state.
+    @states = [
+      :not_started,
+      :started,
+
+      :speech_begin,
+      :speech,
+      :speech_continue,
+
+      :division_begin,
+      :division,
+      :division_continue,
+
+      :recorded_time,
+
+      :heading_begin,
+      :heading,
+
+      :question_by,
+      :question_to,
+      :question,
+      :question_continue,
+
+      :answer_begin,
+      :answer,
+      :answer_continue,
+
+      :resolution,
+      :resolution_continue,
+
+      :narrative,
+      :narrative_continue,
+    ]
+
+    @transitions = {
+      not_started: [:started],
+      started: [:speech],
+
+      speech_begin: [:speech],
+      speech: [:speech_begin, :speech_continue],
+      speech_continue: [:speech_continue],
+
+      division_begin: [:division],
+      division: [:division_continue],
+      division_continue: [:division_continue],
+
+      recorded_time: [:speech],
+
+      heading_begin: [:heading],
+      heading: [:question_by],
+
+      question_by: [:question_to],
+      question_to: [:question],
+      question: [:question_continue],
+      question_continue: [:question_continue],
+
+      answer_begin: [:answer],
+      answer: [:answer_continue],
+      answer_continue: [:answer_continue],
+
+      resolution: [:resolution_continue],
+      resolution_continue: [:resolution_continue],
+
+      narrative: [:narrative_continue],
+      narrative_continue: [:narrative_continue, :speech_begin],
+    }
+
     # A map between speaker names and URLs, for cases where we have only a name,
     # and to have consistent URLs for names.
     @speaker_urls = {}
 
     # A map between URLs and person IDs.
     @speaker_ids = {}
+  end
+
+  def initial_state?
+    @state == @initial_state
+  end
+
+  def can_transition_to?(to)
+    @transitions.key?(@state) && @transitions[@state].include?(to)
+  end
+
+  def transition_to(to)
+    unless can_transition_to?(to)
+      warn "Illegal transition from #{@state} to #{to}"
+    end
+    @state = to
   end
 end
 
@@ -66,6 +151,8 @@ class Speech
   attr_accessor :index
   # @return [String] the Akoma Ntoso element for the paragraph
   attr_accessor :element
+  # @return [Integer] the heading's number
+  attr_accessor :num_title
   # @return [Time] a local time
   attr_accessor :time
   # @return [String] the label for the person speaking
@@ -74,7 +161,7 @@ class Speech
   attr_accessor :html
   # @return [String] a clean version of the paragraph
   attr_accessor :text
-  # @return [Boolean] whether the speech is a voting division
+  # @return [Boolean] whether the speech is a voting division # @todo use element?
   attr_accessor :division
   # @return [Boolean] whether the speaker's name was hyperlinked
   attr_accessor :fuzzy
@@ -83,7 +170,7 @@ class Speech
   # @return [String] the ID of the debate to which this speech belongs
   attr_accessor :debate_id
 
-  dump :index, :element, :time, :from, :html, :text, :division, :fuzzy, :person_id, :debate_id
+  dump :index, :element, :num_title, :time, :from, :html, :text, :division, :fuzzy, :person_id, :debate_id
   foreign_key :debate_id, :person_id
 
   validates_numericality_of :index
