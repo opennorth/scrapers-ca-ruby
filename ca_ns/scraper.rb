b33ca3fcd96b579aa702ea91ac5fd8a322f6413c
James McKinney <james@slashpoundbang.com>
2014-06-18 21:11:24 -0400

    move models to new file

--- ca_ns/scraper.rb
+++ ca_ns/scraper.rb
@@ -115,98 +115,7 @@ class NovaScotia < GovernmentProcessor
   end
 end
 
-class Debate
-  include Pupa::Model
-  include Pupa::Concerns::Timestamps
-  include Pupa::Concerns::Sourceable
-  include ActiveModel::Validations
-
-  attr_accessor :name, :docTitle, :docNumber, :docDate, :docDate_date,
-    :docProponent, :legislature, :legislature_value, :session, :session_value
-  dump :name, :docTitle, :docNumber, :docDate, :docDate_date,
-    :docProponent, :legislature, :legislature_value, :session, :session_value
-
-  def fingerprint
-    to_h.slice(:docNumber)
-  end
-
-  def to_s
-    docTitle
-  end
-end
-
-class Section
-  include Pupa::Model
-  include Pupa::Concerns::Timestamps
-  include ActiveModel::Validations
-
-  attr_accessor :identifier, :heading, :section_id
-  attr_reader :section
-
-  dump :identifier, :heading, :section_id, :section
-
-  foreign_key :section_id
-  foreign_object :section
-
-  validates_presence_of :heading
-
-  def fingerprint
-    to_h.slice(:identifier, :section_id)
-  end
-
-  def to_s
-    heading
-  end
-end
-
-class Speech
-  include Pupa::Model
-  include Pupa::Concerns::Timestamps
-  include ActiveModel::Validations
-
-  # @return [Integer] the index of the paragraph within the debate
-  attr_accessor :index
-  # @return [String] the Akoma Ntoso element for the paragraph
-  attr_accessor :element
-  # @return [Integer] the heading's number
-  attr_accessor :num_title
-  # @return [Time] a local time
-  attr_accessor :time
-  # @return [String] the label for the person speaking
-  attr_accessor :from
-  # @return [String] the ID of the person speaking
-  attr_accessor :from_id
-  # @return [String] the label for the person being spoken to
-  attr_accessor :to
-  # @return [String] the ID of the person being spoken to
-  attr_accessor :to_id
-  # @return [String] the HTML of the paragraph
-  attr_accessor :html
-  # @return [String] a clean version of the paragraph
-  attr_accessor :text
-  # @return [Boolean] whether the speech is a division or a resolution
-  attr_accessor :note
-  # @return [Boolean] whether the speaker's name was hyperlinked
-  attr_accessor :fuzzy
-  # @return [String] the ID of the debate to which this speech belongs
-  attr_accessor :debate_id
-
-  dump :index, :element, :num_title, :time, :from, :from_id, :to, :to_id, :html, :text, :note, :fuzzy, :debate_id
-  foreign_key :debate_id, :from_id
-
-  validates_numericality_of :index
-  validates_inclusion_of :element, in: %w(recordedTime speech narrative other), allow_blank: true
-  validates_presence_of :debate_id
-
-  def fingerprint
-    to_h.slice(:index, :debate_id)
-  end
-
-  def to_s
-    "#{from}: #{html}"
-  end
-end
-
+require_relative 'models'
 require_relative 'constants'
 require_relative 'people'
 require_relative 'speeches'
@@ -218,4 +127,5 @@ runner = Pupa::Runner.new(NovaScotia, {
   database_url: 'mongodb://localhost:27017/sayit',
   expires_in: 604800, # 1 week
 })
+runner.add_action(name: 'akoma_ntoso', description: 'Output speeches as Akoma Ntoso')
 runner.run(ARGV)
