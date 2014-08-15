e8da8225104c08572b3236b8729a1c9b0749c204
James McKinney <james@slashpoundbang.com>
2014-08-15 17:23:24 -0400

    fix options for heroku

--- ca/scraper.rb
+++ ca/scraper.rb
@@ -334,10 +334,22 @@ Canada.add_scraping_task(:conservative)
 Canada.add_scraping_task(:liberal)
 Canada.add_scraping_task(:ndp)
 
-runner = Pupa::Runner.new(Canada, {
-  database_url: 'mongodb://localhost:27017/pupa',
+options = {
+  database_url: ENV['MONGOLAB_URI'] || 'mongodb://localhost:27017/pupa',
   expires_in: 604800, # 1 week
-})
+}
+
+if ENV['REDISCLOUD_URL']
+  options[:output_dir] = ENV['REDISCLOUD_URL']
+end
+
+if ENV['MEMCACHIER_SERVERS']
+  options[:cache_dir] = "memcached://#{ENV['MEMCACHIER_SERVERS']}"
+  options[:memcached_username] = ENV['MEMCACHIER_USERNAME']
+  options[:memcached_password] = ENV['MEMCACHIER_PASSWORD']
+end
+
+runner = Pupa::Runner.new(Canada, options)
 
 runner.add_action(name: 'id', description: 'Set Twitter user IDs, if not set')
 runner.add_action(name: 'update', description: 'Update Twitter screen names')
